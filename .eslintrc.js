21ef12971245b2ac8f36b3e9980de8e1db41e401
pascalwassmer <pascal.wassmer@gmail.com>
2025-07-11 12:18:27 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,41 @@
-
+module.exports = {
+  root: true,
+  env: { 
+    browser: true, 
+    es2020: true 
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.js'],
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: { 
+    react: { 
+      version: '18.2' 
+    } 
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+  },
+}
